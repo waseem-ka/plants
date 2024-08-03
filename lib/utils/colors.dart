0c80dc0f7e9a814400ecf1445599fd99cb3691ce
waseem-ka <kaskas.waseem@gmail.com>
2024-08-03 15:40:16 +0300

    first commit

---
+++ lib/utils/colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class UltColor {
+  static Color blue = Color(0xE6F6F6);
+  static Color purpel = const Color(0x3C2367);
+  static Color pink1 = const Color(0xF6E1D8);
+  static Color pink2 = const Color(0xF3E9F5);
+  static Color pink3 = const Color(0xF3E9F5);
+  static Color grey1 = const Color(0x8C8A8C);
+  static Color grey2 = const Color(0xD9D9D9);
+}
